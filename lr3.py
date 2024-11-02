c01e96ba12b8b969e8adf9bdb244de5aef33fcc8
BessoSonia <besso.sonia@mail.ru>
2024-11-02 15:57:53 +0500

    Обновлены файлы проекта

--- lr3.py
+++ lr3.py
@@ -3,13 +3,13 @@ import json
 import os
 
 
-# token = 'vk1.a.N5Y7h00bQ48qMnKnrBahW_lCiVvyEDhEpoLq9Sar8NZrb1xNBKYw249DDyxT80Nf6XR5KqSGSus3-HS2gmCwzg_86tQ3qeXo0iEOgwnGPemy9jmLueK9u5-mJ_urAacaTaA-tfwg2BJQorIg6oTxlLEjjh9KUJlQMCoaiO__1NTb534wgGmM2kNWkIIaXF1L'
-token = os.getenv("VK_ACCESS_TOKEN")
+token = 'vk1.a.N5Y7h00bQ48qMnKnrBahW_lCiVvyEDhEpoLq9Sar8NZrb1xNBKYw249DDyxT80Nf6XR5KqSGSus3-HS2gmCwzg_86tQ3qeXo0iEOgwnGPemy9jmLueK9u5-mJ_urAacaTaA-tfwg2BJQorIg6oTxlLEjjh9KUJlQMCoaiO__1NTb534wgGmM2kNWkIIaXF1L'
+# token = os.getenv("VK_ACCESS_TOKEN")
 version = 5.131
-user_id = 172531131
+# user_id = 172531131
 
 
-def get_user_data():
+def get_user_data(user_id):
     params = {
         'access_token': token,
         'v': version,
@@ -19,7 +19,7 @@ def get_user_data():
     response = requests.get(f"https://api.vk.com/method/users.get", params=params)
     return response.json()
 
-def get_followers():
+def get_followers(user_id):
     params = {
         'access_token': token,
         'v': version,
@@ -28,7 +28,7 @@ def get_followers():
     response = requests.get(f"https://api.vk.com/method/users.getFollowers", params=params)
     return response.json()
 
-def get_subscriptions():
+def get_subscriptions(user_id):
     params = {
         'access_token': token,
         'v': version,
@@ -37,26 +37,29 @@ def get_subscriptions():
     response = requests.get(f"https://api.vk.com/method/users.getSubscriptions", params=params)
     return response.json()
 
-def save_data(data):
-    with open('output.json', 'w', encoding='utf-8') as f:
+def save_data(file_path, data):
+    with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
     if token:
+        user_id = input("Введите ID пользователя: ")
+        if not user_id:
+            user_id = 172531131
         file_path = input("Введите путь для сохранения файла: ")
         if not file_path:
-            # file_path = "..\output.json"
-            script_dir = os.path.dirname(os.path.realpath(__file__))
-            file_path = os.path.join(script_dir, "output.json")
-        user_data = get_user_data()
-        followers = get_followers()
-        subscriptions = get_subscriptions()
+            file_path = "output.json"
+        else:
+            file_path = file_path + '\output.json'
+        user_data = get_user_data(user_id)
+        followers = get_followers(user_id)
+        subscriptions = get_subscriptions(user_id)
         result = {
             'user_data': user_data,
             'followers': followers,
             'subscriptions': subscriptions
         }
-        save_data(result)
-        print("Данные сохранены в output.json")
+        save_data(file_path, result)
+        print(f"Данные сохранены в {file_path}")
     else:
         print("Ошибка: токен доступа не найден. Установите переменную окружения VK_ACCESS_TOKEN.")
