5d0327403344896e570c2b69290ac6128a6d4c34
BessoSonia <besso.sonia@mail.ru>
2024-11-02 15:47:43 +0500

    Обновлены файлы проекта

--- lr3.py
+++ lr3.py
@@ -1,9 +1,11 @@
 import requests
 import json
+import os
 
 
-token = 'vk1.a.N5Y7h00bQ48qMnKnrBahW_lCiVvyEDhEpoLq9Sar8NZrb1xNBKYw249DDyxT80Nf6XR5KqSGSus3-HS2gmCwzg_86tQ3qeXo0iEOgwnGPemy9jmLueK9u5-mJ_urAacaTaA-tfwg2BJQorIg6oTxlLEjjh9KUJlQMCoaiO__1NTb534wgGmM2kNWkIIaXF1L'
-version = 5.199
+# token = 'vk1.a.N5Y7h00bQ48qMnKnrBahW_lCiVvyEDhEpoLq9Sar8NZrb1xNBKYw249DDyxT80Nf6XR5KqSGSus3-HS2gmCwzg_86tQ3qeXo0iEOgwnGPemy9jmLueK9u5-mJ_urAacaTaA-tfwg2BJQorIg6oTxlLEjjh9KUJlQMCoaiO__1NTb534wgGmM2kNWkIIaXF1L'
+token = os.getenv("VK_ACCESS_TOKEN")
+version = 5.131
 user_id = 172531131
 
 
@@ -40,13 +42,21 @@ def save_data(data):
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
-    user_data = get_user_data()
-    followers = get_followers()
-    subscriptions = get_subscriptions()
-    result = {
-        'user_data': user_data,
-        'followers': followers,
-        'subscriptions': subscriptions
-    }
-    save_data(result)
-    print("Данные сохранены в output.json")
+    if token:
+        file_path = input("Введите путь для сохранения файла: ")
+        if not file_path:
+            # file_path = "..\output.json"
+            script_dir = os.path.dirname(os.path.realpath(__file__))
+            file_path = os.path.join(script_dir, "output.json")
+        user_data = get_user_data()
+        followers = get_followers()
+        subscriptions = get_subscriptions()
+        result = {
+            'user_data': user_data,
+            'followers': followers,
+            'subscriptions': subscriptions
+        }
+        save_data(result)
+        print("Данные сохранены в output.json")
+    else:
+        print("Ошибка: токен доступа не найден. Установите переменную окружения VK_ACCESS_TOKEN.")
